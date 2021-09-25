f7b754e779f1d626d316ac3c2c59ee88aa8cde63
nguyenthanhnghia0907@gmail.com <nguyenthanhnghia0907@gmail.com>
2021-09-26 00:45:19 +0700

    Register

--- src/main/java/com/example/spring_boot_jwt_ntn/controller/AppRestController.java
+++ src/main/java/com/example/spring_boot_jwt_ntn/controller/AppRestController.java
@@ -12,6 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppRestController {
 
+    @Autowired
+    UserService userService;
 
+    @PostMapping("/register")
+    public String saveUserRegister(@RequestBody User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        if(userService.saveAndFlush(user)){
+            return "Register Successful!";
+        }
+        return "Register Fail!";
+    }
+    @GetMapping("/getuser")
+    public User getUser(@RequestBody User user){
+        return userService.getUser(user.getUsername());
+
+    }
 
 }
